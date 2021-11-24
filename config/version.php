f8a73a47c7b284b4266e08fc5ac9551721e7da83
KTUHC <maykythuatuhc@gmail.com>
2021-11-24 14:19:23 +0700

    Update code

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v5.3.1',
+  'full_app_version' => 'v5.3.1 - build 6490-g6690a0f1d',
+  'build_version' => '6490',
+  'prerelease_version' => '',
+  'hash_version' => 'g6690a0f1d',
+  'full_hash' => 'v5.3.1-19-g6690a0f1d',
+  'branch' => 'master',
+);
